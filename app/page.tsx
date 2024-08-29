94cf0b7cc5fc9715cf7b3abbd86345926e9d9e17
Senahid Velazic <senahid.veladzic@elatus.net>
2024-08-29 12:22:18 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import dynamic from "next/dynamic";
 import HeroSection from "@/components/hero-section";
 import {slideComponents} from "@/components/hero-slide";
-
+const ReservationForm = dynamic(
+    () => import("../components/reservation-form"),
+    { ssr: false },
+);
 
 export default function Home() {
   return (
       <main>
           <HeroSection slides={slideComponents}/>
-
+          <section className="container relative z-30 -mt-[100px] mb-5 px-4 xl:px-0">
+              <ReservationForm/>
+          </section>
       </main>
   );
 }
