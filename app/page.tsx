470ca7e6e72870966495c6fe386a3826c539aff3
Senahid Velazic <senahid.veladzic@elatus.net>
2024-08-29 12:18:55 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import HeroSection from "@/components/hero-section";
 import {slideComponents} from "@/components/hero-slide";
-import ReservationForm from "@/components/reservation-form";
+
 
 export default function Home() {
   return (
       <main>
           <HeroSection slides={slideComponents}/>
-          <section className="container relative z-30 -mt-[100px] mb-5 px-4 xl:px-0">
-              <ReservationForm/>
-          </section>
+
       </main>
   );
 }
